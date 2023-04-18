7f6ea402cae0437ebb9e704bad24c0947878e3c5
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-04-18 14:46:41 +0100

    app module optimized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,24 @@
-import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { ManageMembersComponent } from './modules/sidebar/pages/manage-users/manage-members.component';
+import { ContentLayoutComponent } from './layout/content-layout.component';
+import { AnalyticsComponent } from './modules/sidebar/pages/analytics/analytics.component';
+import { ProfileComponent } from './modules/sidebar/pages/profile/profile.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [{
-      path: 'dashboard',
-      component: DashboardComponent
-    }],
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'reset-password',
-    component: ResetPasswordComponent,
-  },
-  {
-    path: '**',
-    pathMatch: 'full',
-    redirectTo: ''
-  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: ContentLayoutComponent, 
+  children: [
+    {path: 'analytics', component: AnalyticsComponent},
+    {path: 'manage-members', component: ManageMembersComponent},
+    {path: 'profile', component: ProfileComponent}
+  ],
+  canActivate: [AuthGuard] },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
-import { HomeComponent } from './components/home/home.component';
-import { AuthService } from './services/auth.service';
+import { AuthService } from './core/services/auth.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,73 +8,49 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ForgotPasswordComponent, LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import {MatIconModule} from '@angular/material/icon';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { EditProfileComponent, ProfileComponent } from './components/profile/profile.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { AddMemberComponent,DashboardComponent } from './components/dashboard/dashboard.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatTableModule} from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatListModule} from '@angular/material/list';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { ManageMembersComponent } from './modules/sidebar/pages/manage-users/manage-members.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { SidebarModule } from './modules/sidebar/sidebar.module';
+import { ContentLayoutModule } from './layout/content-layout.module';
+import { PageNotFoundModule } from './modules/page-not-found/page-not-found.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
-    NavbarComponent,
-    SidebarComponent,
-    ProfileComponent,
-    EditProfileComponent,
-    DashboardComponent,
-    AddMemberComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDialogModule,
     ToastrModule.forRoot({
       closeButton: true,
       timeOut: 3000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
-    MatIconModule,
-    MatMenuModule,
-    MatDividerModule,
-    MatButtonToggleModule,
-    MatProgressBarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatExpansionModule,
-    MatTableModule,
-    MatSortModule,
-    MatCheckboxModule
+    AuthModule,
+    SidebarModule,
+    ContentLayoutModule,
+    PageNotFoundModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
