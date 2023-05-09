7efcf662e8da963a63c55d6c6870e13f0f71a637
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-09 13:09:01 +0100

    event functionalities optimized

--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -13,8 +13,73 @@ export class EventService {
         private router: Router,
         private authService: AuthService) {}
 
+
+    addEvent(data: any) {
+        return this.http.post(ENDPOINT_URL , data, {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    updateEvent(id: any, data: any) {
+        return this.http.put(ENDPOINT_URL + `/${id}`, data, {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    updateStatus() {
+        return this.http.put(ENDPOINT_URL + '/0/update-status', {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    registerEvent(id: any) {
+        return this.http.post(ENDPOINT_URL + `/${id}/register`, {},{
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    deleteEvent(id: any) {
+        return this.http.delete(ENDPOINT_URL + `/${id}` , {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    uploadPhoto(id:any, data: any) {
+        return this.http.put(ENDPOINT_URL + `/${id}` + '/upload', data, {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            },
+        })
+    }
+
     getEvents() {
         return this.http.get(ENDPOINT_URL, {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    getAttendees(id: any) {
+        return this.http.get(ENDPOINT_URL + `/${id}/registrations`, {
+            headers: {
+                Authorization: `Bearer ${this.authService.userToken}`
+            }
+        })
+    }
+
+    getEventById(id: any) {
+        return this.http.get(ENDPOINT_URL + `/${id}`, {
             headers: {
                 Authorization: `Bearer ${this.authService.userToken}`
             }
