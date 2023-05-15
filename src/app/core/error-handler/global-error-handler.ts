9a45e74e30aae5c2eef4d2eaf1fb25fdaba68902
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-15 20:10:59 +0100

    fix: handle unauthorized http error by logging the user out

--- src/app/core/error-handler/global-error-handler.ts
+++ src/app/core/error-handler/global-error-handler.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { NotificationService } from '../services/notification.service';
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorService } from '../services/error.service';
+import { AuthService } from '../services/auth.service';
 
 
 @Injectable()
@@ -13,11 +15,16 @@ export class GlobalErrorHandler implements ErrorHandler {
 
         const errorService = this.injector.get(ErrorService);
         const notifier = this.injector.get(NotificationService);
+        const authService = this.injector.get(AuthService);
 
         let message;
 
         if (error instanceof HttpErrorResponse) {
             // Server Error
+            if(error.status === 401 ) {
+                notifier.showError('Your session is over!');
+                return authService.logout()
+            }
             message = errorService.getServerMessage(error);
             notifier.showError(message);
         } else {
