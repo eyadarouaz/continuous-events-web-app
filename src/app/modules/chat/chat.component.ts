67b413f57a25604d88aee376d251a9d14aecfb60
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-18 08:57:33 +0100

    feat: scroll to the bottom of the chat window to view the latest messages

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/services/auth.service";
 import { ChatService } from "src/app/core/services/chat.service";
 import { UserService } from "src/app/core/services/user.service";
 
@@ -12,22 +13,32 @@ import { UserService } from "src/app/core/services/user.service";
   
     constructor(
       private chatService: ChatService,
-      private userService: UserService
+      private userService: UserService,
+      private authService: AuthService,
     ) {}
 
   messages: Array<any> = [];
   members: Array<any> = [];
   currentUsername = '';
   messageForm = new FormGroup({
-    message: new FormControl('')
+    message: new FormControl('', Validators.required)
   });
 
+  @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
+
+
   ngOnInit() {
+    this.scrollToBottom();    
+    this.authService.currentUser(localStorage.getItem('token'))
+    .subscribe((res: any) => {
+      this.currentUsername = res.data.user.username;
+    })
     this.chatService.getMessages()
     .subscribe((res: any) => {
       res.forEach((element:any) => {
-        this.messages.push({content: element.content, user: element.user})
-      });
+        this.messages.push({content: element.content, user: element.user, 
+          time: new Date(element.createdAt).getHours() + ':' + (new Date(element.createdAt).getMinutes()<10?'0':'')+new Date(element.createdAt).getMinutes()})
+        });
     })
     this.chatService.recieveMessages(this.messages);
 
@@ -41,6 +52,16 @@ import { UserService } from "src/app/core/services/user.service";
         this.members.push({ id: element.id, firstName: element.firstName, image: pic, username: element.username, birthday: element.birthday});
       });
     });
+  }
+
+  ngAfterViewChecked() {        
+    this.scrollToBottom();        
+  } 
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch(err) {}              
   }
   
   sendMessage(form: FormGroup) {
