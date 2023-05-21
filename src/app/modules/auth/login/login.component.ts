e182465e5bf5dae21acdef299af9b557e1a0ea6c
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-21 20:13:45 +0100

    feat: add translation feature to login page

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../../core/services/auth.service';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +12,11 @@ import { ForgotPasswordComponent } from '../forgot-password/forgot-password.comp
 })
 export class LoginComponent {
 
-  constructor(private authService: AuthService, public dialog: MatDialog) {}
-
+  constructor(private authService: AuthService, public dialog: MatDialog, public translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+    translate.use(localStorage.getItem('lang')+"");
+  }
 
   login(loginForm: NgForm) {
     if (loginForm.invalid) {
@@ -24,6 +28,14 @@ export class LoginComponent {
 
   openDialog() {
     this.dialog.open(ForgotPasswordComponent);
+  }
+
+  changeLanguage(value: any) {
+    localStorage.setItem('lang', value)
+  }
+
+  getCurrentLanguage(lang: string) {
+    return localStorage.getItem('lang') === lang;
   }
 
 }
