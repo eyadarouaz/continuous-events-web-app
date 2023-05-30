b11967241b127f74f492a3810bc7af944463d061
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-30 20:47:49 +0100

    perf: send client token via query data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { ToastrModule } from 'ngx-toastr';
-import { environment as env } from "src/environment";
+import { environment as env } from "./../environment";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalErrorHandler } from './core/error-handler/global-error-handler';
@@ -17,12 +19,10 @@ import { MemberProfileModule } from './modules/member-profile/member-profile.mod
 import { PageNotFoundModule } from './modules/page-not-found/page-not-found.module';
 import { PollPageModule } from './modules/poll-page/poll-page.module';
 import { SidebarModule } from './modules/sidebar/sidebar.module';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 const config: SocketIoConfig = { url: env.apiBaseUrl, options: {
-  extraHeaders: {
-    Authorization: localStorage.getItem('token')+" "
+  query: {
+    token: localStorage.getItem('token')
   }
 } };
 
