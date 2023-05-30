ae91fc07a745f3da78c4024f92313414228daa2d
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-30 20:49:48 +0100

    feat: add path to not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,29 @@
-import { LoginComponent } from './modules/auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
-import { ManageMembersComponent } from './modules/sidebar/pages/manage-users/manage-members.component';
-import { ContentLayoutComponent } from './layout/content-layout.component';
-import { AnalyticsComponent } from './modules/sidebar/pages/analytics/analytics.component';
-import { ProfileComponent } from './modules/sidebar/pages/profile/profile.component';
-import { AuthGuard } from './core/guards/auth.guard';
-import { ManageEventsComponent } from './modules/sidebar/pages/manage-events/manage-events.component';
-import { FeedComponent } from './modules/sidebar/pages/feed/feed.component';
 import { AdminGuard } from './core/guards/admin.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 import { ProfileGuard } from './core/guards/profile.guard';
-import { MemberProfileComponent } from './modules/member-profile/member-profile.component';
+import { ContentLayoutComponent } from './layout/content-layout.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 import { ResetPasswordComponent } from './modules/auth/reset-password/reset-password.component';
-import { EventsComponent } from './modules/sidebar/pages/events/events.component';
-import { EventPageComponent } from './modules/event-page/event-page.component';
-import { ManagePollsComponent } from './modules/sidebar/pages/manage-polls/manage-polls.component';
-import { PollsComponent } from './modules/sidebar/pages/polls/polls.component';
-import { PollPageComponent } from './modules/poll-page/poll-page.component';
 import { ChatComponent } from './modules/chat/chat.component';
+import { EventPageComponent } from './modules/event-page/event-page.component';
+import { MemberProfileComponent } from './modules/member-profile/member-profile.component';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { PollPageComponent } from './modules/poll-page/poll-page.component';
+import { AnalyticsComponent } from './modules/sidebar/pages/analytics/analytics.component';
+import { EventsComponent } from './modules/sidebar/pages/events/events.component';
+import { FeedComponent } from './modules/sidebar/pages/feed/feed.component';
+import { ManageEventsComponent } from './modules/sidebar/pages/manage-events/manage-events.component';
+import { ManagePollsComponent } from './modules/sidebar/pages/manage-polls/manage-polls.component';
+import { ManageMembersComponent } from './modules/sidebar/pages/manage-users/manage-members.component';
+import { PollsComponent } from './modules/sidebar/pages/polls/polls.component';
+import { ProfileComponent } from './modules/sidebar/pages/profile/profile.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
   { path: '', redirectTo:'home', pathMatch: 'full' },
   { path: '', component: ContentLayoutComponent, 
   children: [
@@ -41,8 +41,6 @@ const routes: Routes = [
     {path: ':username', component: MemberProfileComponent, canActivate: [ProfileGuard]},
   ],
   canActivate: [AuthGuard] },
-  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
-  
 ];
 
 @NgModule({
