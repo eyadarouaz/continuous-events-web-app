9b28e16d60402f8bc589f1b86c56eaf044a94949
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-30 20:24:37 +0100

    feat: disconnect socket when user logs out

--- src/app/layout/content-layout.component.ts
+++ src/app/layout/content-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
+import { Socket, SocketIoConfig } from "ngx-socket-io";
+import { environment as env } from "src/environment";
 import { AuthService } from "./../core/services/auth.service";
 
 @Component({
@@ -6,9 +8,23 @@ import { AuthService } from "./../core/services/auth.service";
     templateUrl: './content-layout.component.html',
     styleUrls: []
   })
-export class ContentLayoutComponent {
+export class ContentLayoutComponent implements OnDestroy{
 
-  constructor(private authService: AuthService) {}
+  config: SocketIoConfig = { url: env.apiBaseUrl, options: {
+    query: {
+      token: localStorage.getItem('token')
+    }
+  } };
+
+  socket: any
+
+  constructor(private authService: AuthService) {
+    this.socket = new Socket(this.config)
+  }
+
+  ngOnDestroy(): void {
+    this.socket.disconnect()
+  }
 
   logout() {
     this.authService.logout();
