098083123ee979b2e6fa5dd569b80a43349a7e8f
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-30 20:28:41 +0100

    perf: set default language to english if no language is selected

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
-import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +15,7 @@ export class LoginComponent {
   constructor(private authService: AuthService, public dialog: MatDialog, public translate: TranslateService) {
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang('en');
-    translate.use(localStorage.getItem('lang')+"");
+    translate.use(localStorage.getItem('lang')? localStorage.getItem('lang')+"" : "en");
   }
 
   login(loginForm: NgForm) {
