a09910069d89bbf4e6bfc39a48d21f15b77def34
eya.darouaz <eya.darouaz@intern.continuousnet.com>
2023-05-30 20:34:27 +0100

    feat: check if current user is registered to the event

--- src/app/modules/event-page/event-page.component.ts
+++ src/app/modules/event-page/event-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from './../../core/services/auth.service';
 import { EventService } from './../../core/services/event.service';
+import { NotificationService } from './../../core/services/notification.service';
 
 @Component({
   selector: 'app-event-page',
@@ -10,6 +12,7 @@ import { EventService } from './../../core/services/event.service';
 })
 export class EventPageComponent implements OnInit {
   
+  currentUser = 0;
   editPhotoForm!: FormGroup;
   role: string | null = '';
   id = '';
@@ -24,12 +27,15 @@ export class EventPageComponent implements OnInit {
   status = '';
   image = 'assets/images/default-event.jpg';
   memberImage = 'assets/images/default_profile_image.webp'
-  registered = false;
+  isRegistered = false;
+  clicked = false;
   
   constructor(
     private eventService: EventService,
     public route: ActivatedRoute,
-    private formBuilder: FormBuilder){ 
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private notificationService: NotificationService){ 
       this.eventService.updateStatus(); 
   }
 
@@ -40,16 +46,23 @@ export class EventPageComponent implements OnInit {
         this.id = res.data.id;
         this.title = res.data.title;
         this.description = res.data.description;
-        this.start = new Date(res.data.startDate).toDateString();
-        this.end = new Date(res.data.endDate).toDateString();
+        this.start = new Date(res.data.startDate).toDateString()+ ' at ' + new Date(res.data.startDate).getHours() + ':' +new Date(res.data.startDate).getMinutes()+(new Date(res.data.startDate).getMinutes()<10?'0':'');
+        this.end = new Date(res.data.endDate).toDateString()+ ' at ' + new Date(res.data.endDate).getHours() + ':' +new Date(res.data.endDate).getMinutes()+(new Date(res.data.endDate).getMinutes()<10?'0':'');
         this.location = res.data.location;
         this.status = res.data.status;
         if(res.data.image) this.image = `http://localhost:3000/event/${this.id}/image`;
 
         this.eventService.getAttendees(this.id)
         .subscribe((res: any)=> {
-            this.attendees = res.data.list;
-            this.attendeesNumber = this.attendees.length-3
+          this.attendees = res.data.list;
+          this.attendeesNumber = this.attendees.length-3;
+          this.authService.currentUser(localStorage.getItem('token'))
+          .subscribe((res: any) => {
+            this.currentUser = res.data.user.id;
+            if (this.attendees.some(element => element.user.id === this.currentUser)){
+              this.isRegistered = true;
+            }
+          })
         });
 
         const splitDate = this.start.split(' ');
@@ -76,7 +89,8 @@ export class EventPageComponent implements OnInit {
   }
 
   register() {
-    this.eventService.registerEvent(this.route.snapshot.paramMap.get('id'));
+    this.clicked = true;
+    this.eventService.registerEvent(this.route.snapshot.paramMap.get('id'))
+    .subscribe(() => this.notificationService.showSuccess('Registered successfully'));
   }
-    
 }
